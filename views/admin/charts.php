764edea6caae204ea199d1a5fb40a4eecda44e31
Aaron <116898672+acurro9@users.noreply.github.com>
2024-02-24 14:15:38 +0000

    Update charts.php

--- views/admin/charts.php
+++ views/admin/charts.php
@@ -3,9 +3,7 @@
 </script>
 <link rel="stylesheet" href="../build/css/charts.css">
 <main>
-<div id="chart_div_Aaron"></div>
-    <div id="chart_div_Aaron2"></div>
-    <div id="chart_div_Aaron3"></div>
+    <div id="chart_div_Aaron"></div>
 </main>
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script type="text/javascript" src="../build/js/googleCharts.js"></script>
